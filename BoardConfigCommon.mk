99e648a0e4b45e7709bd62583a8ef8b4f6e56991
Peter Cai <peter@typeblog.net>
2021-10-12 09:12:40 -0400

    add bdroid config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,8 @@
 DEVICE_PATH := device/peter/gsi
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
 # Sepolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
