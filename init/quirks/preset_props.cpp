e759d9cc2ce6a3820912cc6648296538cbc5d72d
Peter Cai <peter@typeblog.net>
2024-12-21 11:05:10 -0500

    gsi: init: Treat virtual sensors as real on Unihertz Jelly Max

--- init/quirks/preset_props.cpp
+++ init/quirks/preset_props.cpp
@@ -10,10 +10,12 @@ PRESET_PROPS(
 
 // Unihertz Jelly Max: Broken "fake" gravity sensor. Ignore it to use AOSP's SensorFusion
 // Also ignore broken inbuilt vibrator effects
+// Also it uses "/virtual" instance name for real biometric sensors, for some reason.
 PRESET_PROPS(
     FP_STARTS_WITH("Unihertz/Jelly_Max"),
     "persist.sys.phh.sensor_filter", "mtk:gravity",
-    "persist.sys.phh.ignore_vibrator_effects", "true"
+    "persist.sys.phh.ignore_vibrator_effects", "true",
+    "persist.sys.phh.virtual_sensors_are_real", "true"
 );
 
 // Some xiaomi devices: Override default network mode to enable 5G
