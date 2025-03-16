5ee1973ddfa4648d4ec1a131e618e45993a63a52
Peter Cai <peter@typeblog.net>
2025-03-16 13:47:49 -0400

    gsi: Rename keylayouts so they don't get picked up automatically

--- quirks/quirks.mk
+++ quirks/quirks.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := device/peter/gsi/quirks
 
 # Keylayout quirks
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/unihertz-fingerprint_key.kl:$(TARGET_COPY_OUT_SYSTEM_EXT)/quirks/keylayout/unihertz-fingerprint_key.kl \
-    $(LOCAL_PATH)/keylayout/unihertz-mtk-kpd.kl:$(TARGET_COPY_OUT_SYSTEM_EXT)/quirks/keylayout/unihertz-mtk-kpd.kl \
-    $(LOCAL_PATH)/keylayout/unihertz-mtk-tpd.kl:$(TARGET_COPY_OUT_SYSTEM_EXT)/quirks/keylayout/unihertz-mtk-tpd.kl \
+    $(LOCAL_PATH)/keylayout/unihertz-fingerprint_key.kl.raw:$(TARGET_COPY_OUT_SYSTEM_EXT)/quirks/keylayout/unihertz-fingerprint_key.kl \
+    $(LOCAL_PATH)/keylayout/unihertz-mtk-kpd.kl.raw:$(TARGET_COPY_OUT_SYSTEM_EXT)/quirks/keylayout/unihertz-mtk-kpd.kl \
+    $(LOCAL_PATH)/keylayout/unihertz-mtk-tpd.kl.raw:$(TARGET_COPY_OUT_SYSTEM_EXT)/quirks/keylayout/unihertz-mtk-tpd.kl \
